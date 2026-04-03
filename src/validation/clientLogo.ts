ee9bc52cb5edf68a27c5b02cf2117545a4260a86
hk-krish <krishhkdigiverse@gmail.com>
2026-04-03 11:47:31 +0530

    feat: add Joi validation schemas for client logo creation, editing, and retrieval

--- src/validation/clientLogo.ts
+++ src/validation/clientLogo.ts
@@ -5,7 +5,7 @@ import { IClientLogo, ICommonGetValidate } from "../type";
 export const addClientLogoSchema = Joi.object<IClientLogo>({
   name: Joi.string().required(),
   image: Joi.string().required(),
-  link: Joi.string().required(),
+  link: Joi.string().optional(),
   ...baseApiSchema,
 });
 
@@ -13,7 +13,7 @@ export const editClientLogoSchema = Joi.object<IClientLogo>({
   clientLogoId: objectId().required(),
   name: Joi.string().optional(),
   image: Joi.string().optional().allow("", null),
-  link: Joi.string().optional(),
+  link: Joi.string().optional().allow("", null),
   ...baseApiSchema,
 });
 
